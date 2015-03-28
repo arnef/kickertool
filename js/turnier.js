96c8c8fabfb4dd91e612504c76bb67d66f742058
arnef <git@arnefeil.de>
2015-03-28 13:36:55 +0100

    dialogs beim turniermodus

--- js/turnier.js
+++ js/turnier.js
@@ -1,17 +1,15 @@
 (function () {
     var app = angular.module('kturnier', []);
     
-    app.controller('TurnierController', function($scope, $filter, $location, DataService) {
+    app.controller('TurnierController', function($scope, $filter, $location, dialogs, DataService) {
         var _self = this;
         
         $scope.tables = DataService.tables;
-        $scope.ranking = DataService.ranking || DataService.teams;
+        $scope.ranking = DataService.teams;
         $scope.matches = [];
         $scope.playedMatches = [];
         _self.currentRound = 0;
-        console.log($scope.tables);
-        console.log($scope.raking);
-        
+                
         _self.selectedMatch = null;
         
         $scope.startRound = function() {
@@ -32,15 +30,32 @@
         };
         $scope.startRound();
         
-        _self.tableIndex;
         $scope.insertScore = function(tableIndex) {
-            if (_self.tables[tableIndex].score != null) {
-                _self.tableIndex = tableIndex;
-                _self.selectedMatch = _self.tables[tableIndex];
-                _self.selectedMatch.score = {};
-                var instance = $modal.open({
-                    templateUrl: 'insertScore.html',
-                    controller: 'ModalScoreController',
+            if ($scope.tables[tableIndex].score != null) {
+                var match = $scope.tables[tableIndex];
+                var dlg = dialogs.create('templates/result_dialog.html', 'InsertResultDialogController', match, {size: 'sm'});
+                dlg.result.then(function(score) {
+                    if (score == 1) {
+                        match.team1.points += 2;
+                        match.score = {team1: 2, team2: 0};
+                    }
+                    if (score == 2) {
+                        match.team1.points += 1;
+                        match.team2.points += 1;
+                        match.score = { team1: 1, team2: 1};
+                    }
+                    if (score == 3) {
+                        match.team2.points += 2;
+                        match.score = { team1: 0, team2: 2};
+                    }
+                    $scope.playedMatches.push(match);
+                    if ($scope.matches.length == 0) {
+                        $scope.tables[tableIndex] = {};
+                    }
+                    else {
+                        $scope.tables[tableIndex] = $scope.matches.pop();
+                    }
+                    $scope.ranking = $filter('orderBy')($scope.ranking, '-points');
                 });
             }
         };
@@ -87,5 +102,19 @@
             DataService.ranking = $filter('orderBy')(_self.ranking, '-points');
             $location.path('ko');
         };
+    });
+    
+    app.controller('InsertResultDialogController', function($scope, $modalInstance, data) {
+        
+        $scope.match = data;
+        
+        $scope.saveResult = function(index) {
+            $modalInstance.close(index);
+        };
+        
+        $scope.cancel = function(){
+			$modalInstance.dismiss('Canceled');
+		};
+        
     });
 })();
