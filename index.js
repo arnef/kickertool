4af651b86600745977472bc3d18946a5528f7065
Arne <git@arnefeil.de>
2015-12-10 23:36:38 +0100

    more electron stuff

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var app = require('app');
 var Window = require('browser-window');
-
+var Menu = require('menu');
 app.on('ready', function () {
   var mainWindow = new Window({
     width: 1100,
     height: 600,
     title: 'Kickertool',
-    frame: false,
+    frame: true,
     icon: 'icon.png',
     center: true
   });
   mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+  //Menu.setApplicationMenu(null);
+
 });
