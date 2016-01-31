6700af34d891406351c2bb05aceeba5d14bc04c6
Arne <git@arnefeil.de>
2016-01-31 16:14:40 +0100

    stuff

--- app/src/insert/doubles/doubles.directive.js
+++ app/src/insert/doubles/doubles.directive.js
@@ -1,10 +1,16 @@
 (function () {
   'use strict';
-
+  console.log('DoublesDirective');
   angular.module('app')
     .directive('doubles', DoublesDirective);
 
   function DoublesDirective() {
-
+    return {
+      templateUrl: 'kickertool-tpls/doubles.view.html',
+      controllerAs: 'doublesCtrl',
+      controller: ['$scope', '$attrs', function ($scope, $attrs) {
+        console.log('Doubles');
+      }]
+    };
   }
 })();
